2115c5bff4c28fe7943a017f8cdeaa0d4c0ce411
Peter LeCavalier <pele6150@colorado.edu>
2022-04-26 17:18:31 -0600

    Finishing touches on code!

--- img_manip.py
+++ img_manip.py
@@ -10,22 +10,17 @@ from cv2 import copyMakeBorder, BORDER_REFLECT
 import math
 import timeit
 
-
-
-# convolution - edge detection, blurring
-
-# rotation: [cos, -sin]
-#           [sin, cos]
-
 def rotation(im, theta):
     # im as a np array, theta in degrees
     theta = np.radians(theta)
     # Set up the rotation matrix to be applied to each pixel
     rot_mat = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
 
+    # max of each axis
     y_max = im.shape[0] - 1
     x_max = im.shape[1] - 1
 
+    # Calculate the lower and upper bounds across each axis when rotation is applied
     bounds = []
     bounds.append(np.matmul(rot_mat, [0, 0]))
     bounds.append(np.matmul(rot_mat, [0, y_max]))
@@ -34,6 +29,7 @@ def rotation(im, theta):
 
     bounds = np.array(bounds)
 
+    # low and high bounds here - (x, y) for each
     low_bounds = np.min(bounds, axis=0)
     high_bounds = np.max(bounds, axis=0)
     
@@ -64,22 +60,24 @@ def rotation(im, theta):
     # Set up a new matrix with each entry being the new pixel placement
     new_px_mat = np.ones((new_y_size, new_x_size, im.shape[2])).astype(int) + 254
 
-    #make an array
+    # Make a meshgrid of coordinate values to apply rotation to
     xs, ys = np.meshgrid(np.arange(im.shape[1]), np.arange(im.shape[0]))
     idxs = np.dstack((xs, ys)).reshape((xs.shape[0], xs.shape[1], 2))
     idxs = idxs.reshape((idxs.shape[0] * idxs.shape[1], 2))
     idxs = np.swapaxes(idxs, 0, 1)
+    
+    # Rotate the indices by matrix multiplying by the rot. matrix
     rot_idxs = np.matmul(rot_mat, idxs)
     rot_idxs[0] = np.clip(rot_idxs[0] + left_shift, 0, new_x_size - 1)
     rot_idxs[1] = np.clip(rot_idxs[1] + up_shift, 0, new_y_size - 1)
     rot_idxs = np.swapaxes(rot_idxs, 0, 1).astype(int)
     
+    # Loop through each of the pixels and set the new image
     for y in range(len(im)):
         for x in range(len(im[y])):
             new_x, new_y = rot_idxs[y*len(im[y]) + x].astype(int)
             # shift the pixel values to the location on the new image
             # clip to avoid out-of-bounds indexing
-            #new_px_mat[np.clip(new_y + up_shift, 0, new_y_size - 1), np.clip(new_x + left_shift, 0, new_x_size - 1)] = im[y, x]
             new_px_mat[new_y, new_x] = im[y, x]
     return new_px_mat
 
@@ -88,6 +86,7 @@ def rotation(im, theta):
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
+# Create a gaussian smoothing kernel with a specific size
 def gaussian_kernel(size=5):
     # radius of the kernel - used to conveniently get the right index
     radius = int(np.floor(size / 2))
@@ -96,7 +95,7 @@ def gaussian_kernel(size=5):
     #coefficient to go in front of each exp
     coef = (1/(2*math.pi*sigma**2))
 
-    # make an array
+    # make an array of coordinates
     # doing it this way will make matrix math a lot more efficient
     xs, ys = np.meshgrid(np.arange(-radius, radius+1), np.arange(-radius, radius+1))
     x2_plus_y2 = np.square(xs) + np.square(ys)
@@ -121,20 +120,19 @@ for i in im_filenames:
     cur_im = np.array(Image.open(i)).astype(int)
     ims.append(cur_im)
 
+# Make variables for each
 wall_arr = ims[0]
 bottle_arr = ims[1]
 acne_arr = ims[2]
 smile_arr = ims[3]
 
 
-# Rotation
-"""
-wall_arr = rotation(wall_arr, 90)
-bottle_arr = rotation(bottle_arr, 90)
-"""
-rot_wall_arr = np.rot90(wall_arr, 3)
-rot_bottle_arr = np.rot90(bottle_arr, 3)
+# ------ Rotation
+# Apply a 270 degree rotation on the images
+rot_wall_arr = rotation(wall_arr, 90)
+rot_bottle_arr = rotation(bottle_arr, 90)
 
+# Plot everything
 fig, axs = plt.subplots(2, 2)
 axs[0][0].imshow(wall_arr)
 axs[0][0].set_title("Original Wall image", fontsize=20)
@@ -146,10 +144,13 @@ axs[1][1].imshow(rot_bottle_arr)
 axs[1][1].set_title("Rotated Bottle image", fontsize=20)
 # ------ END Rotation
 
-# Subtraction
+# ------ Subtraction
+# Take the absolute value of the subtraction between the images
+# and clip values to stay in 0-255
 subtract = 255 - np.abs(np.subtract(rot_bottle_arr, rot_wall_arr))
 subtract = np.clip(subtract, 0, 255)
 
+# Plot everything
 fig, axs = plt.subplots(1, 3, sharey=True, sharex=True)
 axs[0].imshow(rot_bottle_arr)
 axs[1].imshow(rot_wall_arr)
@@ -159,9 +160,12 @@ axs[1].set_title("Wall", fontsize=20)
 axs[2].set_title("Absolute value of wall img\nsubtracted from bottle img.", fontsize=20)
 # ------ END Subtraction
 
-# Scalar Multiplication
+# ------ Scalar Multiplication
+
+# Scalar multiply the image
 darker_im = (0.4 * subtract).astype(int)
 
+# Plot everything
 fig, axs = plt.subplots(1, 2, sharey=True, sharex=True)
 axs[0].imshow(subtract)
 axs[1].imshow(darker_im)
@@ -169,34 +173,31 @@ axs[0].set_title("Water bottle subtracted image", fontsize=20)
 axs[1].set_title("Scalar multiplied image (x0.4)", fontsize=20)
 # ------ END Scalar Multiplication
 
-plt.show()
-
-
-
-# Convolution
-
-# Edge detection
+# ------ Edge detection
+# Turn the image into grayscale
 smile_gray = rgb2gray(smile_arr)
+
+# Create a gaussian kernel
 kernel_size = 5
 kernel_radius = int(np.floor(kernel_size / 2))
 smooth_55_kernel = gaussian_kernel(kernel_size)
 
-
+# This times the processing
 start = timeit.default_timer()
 
+# Apply a gaussian smoothing to the image
 smooth_smile = convolve2d(smile_gray, smooth_55_kernel, boundary="symm")
-
-stop = timeit.default_timer()
-
-print(f"convolve2d: {stop - start}")
-
 
 # Crop the image to remove padding during the convolution process
 smooth_smile = smooth_smile[kernel_radius:-kernel_radius, kernel_radius:-kernel_radius]
 
+# Subtract the smoothed image from the original and threshold
 edge = smile_gray - smooth_smile
 edge = edge < -5
+stop = timeit.default_timer()
+print(f"Edge detection time: {stop - start} seconds")
 
+# Plot everything
 fig, axs = plt.subplots(1, 3, sharey=True, sharex=True)
 axs[0].imshow(smile_gray, cmap="gray")
 axs[1].imshow(smooth_smile, cmap="gray")
@@ -204,38 +205,40 @@ axs[2].imshow(edge, cmap="gray")
 axs[0].set_title("Original image", fontsize=20)
 axs[1].set_title("Gaussian smoothed image", fontsize=20)
 axs[2].set_title("Edge detected image", fontsize=20)
-
-plt.show()
 # ------ END Edge detection
 
-
+# ------ Gaussian Smoothing
 # Set the kernel size/radius
 kernel_size = 61
 kernel_radius = int(np.floor(kernel_size / 2))
-# Generate the kernel (expand_dims adds an extra dimension for convolution)
+# Generate a gaussian kernel (expand_dims adds an extra dimension for convolution)
 smooth_55_kernel = np.expand_dims(gaussian_kernel(kernel_size), axis=-1)
 
 # Focus on a specific portion of the image
 smooth_acne = np.copy(acne_arr)
-acne_crop = smooth_acne[200:1800, 150:2000]#[1300:1900, 775:1550]
+acne_crop = smooth_acne[200:1800, 150:2000]
 
 # Add borders to the image for convolution, reflecting the borders
 acne_crop = copyMakeBorder(acne_crop, kernel_radius, kernel_radius, kernel_radius, kernel_radius, BORDER_REFLECT)
 
+# Time the processing
 start = timeit.default_timer()
 
+# Apply gaussian smoothing to the image
 smooth_acne_crop = convolve(acne_crop, smooth_55_kernel).astype(int)
 
-stop = timeit.default_timer()
-
-print(f"Convolution: {stop - start}")
-
+# Remove padding after convolution
 smooth_acne_crop = smooth_acne_crop[kernel_size - 1:-kernel_size + 1, kernel_size - 1:-kernel_size + 1]
 
+# Replace the section of the image with the convolved area
 smooth_acne[200:1800, 150:2000] = smooth_acne_crop
+stop = timeit.default_timer()
+print(f"Gaussian Smoothing timing: {stop - start} seconds")
 
+# Create a rectangle to draw over the smoothing area
 rect = Rectangle((150,200),1850,1600, edgecolor='r', facecolor="none")
 
+# Plot everything
 fig, axs = plt.subplots(1, 3, sharey=True, sharex=True)
 axs[0].imshow(acne_arr)
 axs[1].imshow(acne_arr)
@@ -245,5 +248,7 @@ axs[2].imshow(smooth_acne)
 axs[0].set_title("Original acne image", fontsize=20)
 axs[1].set_title("Box highlighting convolution area", fontsize=20)
 axs[2].set_title("Gaussian smoothed acne image", fontsize=20)
+# ------ END Gaussian Smoothing
 
+# Show all the plots
 plt.show()
